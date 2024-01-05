1969359f1265cbed394d195eccbc81748c5ba221
Jesus <94246161+jbinade@users.noreply.github.com>
2024-01-05 13:22:50 +0100

    mejoras

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const { src, dest, watch, parallel } = require('gulp');
 // CSS
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 // Imagenes
 const cache = require('gulp-cache');
@@ -10,10 +14,16 @@ const imagemin = require('gulp-imagemin');
 const webp = require("gulp-webp");
 const avif = require('gulp-avif');
 
+// Javascript
+const terser = require('gulp-terser-js');
+
 function css(done) {
     src('src/scss/**/*.scss') // Indentificar el archivo de sass
+        .pipe(sourcemaps.init())
         .pipe( plumber())
         .pipe( sass() ) // Compilarlo
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe(sourcemaps.write('.'))
         .pipe( dest("build/css") ) // Almacenarlo
     done(); // Callback que avisa a gulp cuando llegamos al final
 }
@@ -51,6 +61,9 @@ function versionAvif( done ) {
 
 function javascript( done ) {
     src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe( terser() )
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
 
     done();
